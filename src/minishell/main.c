31f5660d9c980f23036c1438b0c8036958c46424
eugene-png <95448366+eugene-png@users.noreply.github.com>
2024-05-11 19:29:12 +0200

    ADD main program loop

--- src/minishell/main.c
+++ src/minishell/main.c
@@ -6,7 +6,7 @@
 /*   By: erybolov <erybolov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/11 17:35:00 by erybolov          #+#    #+#             */
-/*   Updated: 2024/05/11 18:09:50 by erybolov         ###   ########.fr       */
+/*   Updated: 2024/05/11 18:35:59 by erybolov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 int main(int argc, char **argv)
 {
-	t_minishell	*m;
+	static t_minishell	*m;
 
 	m = malloc(sizeof(t_minishell));
 	minishell_run(m);
@@ -22,6 +22,9 @@ int main(int argc, char **argv)
 }
 
 /* m as variable name because later we would like to work with nested structs like m->some->struct->another
+ * m is static so we don't have to set fields as NULL
+ *
  * minishell_run is a program loop that will interact with user
+ *
  * also we have to implement cleanup (free etc) before we exit from main
  */
--- src/minishell/minishell.h
+++ src/minishell/minishell.h
@@ -6,7 +6,7 @@
 /*   By: erybolov <erybolov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/11 17:39:34 by erybolov          #+#    #+#             */
-/*   Updated: 2024/05/11 18:05:25 by erybolov         ###   ########.fr       */
+/*   Updated: 2024/05/11 18:34:49 by erybolov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,14 @@
 # define MINISHELL_H
 
 #include "../../libft/libft.h"
+#include <readline/readline.h>
+#include <readline/history.h>
 
 typedef struct s_minishell
 {
 	t_list	*input_history;
 }	t_minishell;
 
-void	minishell_run(t_minishell m);
+void	minishell_run(t_minishell *m);
 
 #endif
