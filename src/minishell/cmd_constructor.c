7cb38e60cfb0415471f7047812d3818d959dad37
eugene-png <95448366+eugene-png@users.noreply.github.com>
2024-05-21 02:56:18 +0200

    FIX naming

--- src/minishell/cmd_constructor.c
+++ src/minishell/cmd_constructor.c
@@ -6,13 +6,13 @@
 /*   By: corin <corin@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/13 10:09:33 by corin             #+#    #+#             */
-/*   Updated: 2024/05/13 10:21:08 by corin            ###   ########.fr       */
+/*   Updated: 2024/05/21 02:55:23 by erybolov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-t_cmd*	execcmd(void)
+t_cmd*	create_exec_cmd(void)
 {
 	t_cmd *cmd;
 
@@ -24,7 +24,7 @@ t_cmd*	execcmd(void)
 	return(t_cmd*)cmd;
 }
 
-t_cmd*	redircmd(t_cmd *subcmd, char *file, char *efile, int mode, int fd)
+t_cmd*	create_redir_cmd(t_cmd *subcmd, char *file, char *efile, int mode, int fd)
 {
 	t_redircmd	*cmd;
 	
@@ -40,7 +40,7 @@ t_cmd*	redircmd(t_cmd *subcmd, char *file, char *efile, int mode, int fd)
 	cmd->fd = fd;
 	return (t_cmd*)cmd;
 }
-t_cmd *pipecmd(t_cmd *left, t_cmd *right)
+t_cmd *create_pipe_cmd(t_cmd *left, t_cmd *right)
 {
 	t_pipecmd *cmd;
 
